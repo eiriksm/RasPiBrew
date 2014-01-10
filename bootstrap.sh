7f6f464d76ce7269eeab02ad2268254f05bafa14
Eirik S. Morland <eirik@morland.no>
2014-01-10 18:15:35 +0100

    First version of simple vagrant dev env.

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+# Exit if setup is already done (Vagrant 1.2.x backward compatibility).
+if [ -e "/opt/vagrant-installed" ]; then
+    exit 0
+fi
+mkdir /opt/vagrant-installed
+apt-get install python-smbus vim python-setuptools -y
+easy_install web.py pyserial
+
+rm -rf /var/www
+
+ln -s /vagrant /var/www
